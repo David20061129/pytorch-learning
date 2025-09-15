30a474df83fe23f6bcb3799984dbe185707727bc
David ren <rzj24@mails.tsinghua.edu.cn>
2025-09-15 16:22:17 +0800

    Update transformer.py

--- .idea/transformer.py
+++ .idea/transformer.py
@@ -1,7 +1,12 @@
 from torchvision import transforms
 from PIL import Image
+import cv2
+
 path = "data\\hymenoptera_data\\train\\ants\\0013035.jpg"
+numpy_picture = cv2.imread(path)
+print(numpy_picture)
 img = Image.open(path)
 tens = transforms.ToTensor()
 to = tens(img)
 print(to)
+print(tens(numpy_picture))
